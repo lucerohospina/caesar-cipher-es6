e6e65858365af975a7e2e7d3ed55d8238a0092dc
lucerohospina <lucerojhp@gmail.com>
2018-01-26 03:46:38 -0500

    agregando funcionalidad con es6

--- js/app.js
+++ js/app.js
@@ -1,70 +1,85 @@
+// Declarando variables
 let cipherWord = document.getElementById('cipher-input');
 let cipherBtn = document.getElementById('cipher-btn');
 let decipherWord = document.getElementById('decipher-input');
 let decipherBtn = document.getElementById('decipher-btn');
+let validateWord = false;
+let validateWord2 = false;
 
+// Añadiendo eventos
 cipherBtn.addEventListener('click', cipher);
 decipherBtn.addEventListener('click', decipher);
 
-function cipher() {
-  console.log('click');
-  //  usuario introduce una palabra en el input
+// Funciones de los input
+cipherWord.addEventListener('keyup', () => {
   console.log(cipherWord.value);
-  // empieza un ciclo para recorrer indice por indice la frase introducida
-  for (var i = 0; i < cipherWord.value.length; i++) {
-    // condicionamos el recorrido del ciclo a que si encuentra número este se detenga y vuelva a preguntar por la frase a ser usada
-    if (parseInt(cipherWord.value[i]) % 1 === 0) {
-      alert('Introduza las palabras con solo letras por favor');
+  // validamos que lo ingresado en el input no sea un numero y que no este vacio
+  if (cipherWord.value === '' || parseInt(cipherWord.value) % 1 === 0) {
+    cipherBtn.setAttribute('disabled', true);
+  } else {
+    cipherBtn.removeAttribute('disabled');
+  }
+}); 
+
+decipherWord.addEventListener('keyup', () => {
+  console.log(decipherWord.value);
+  // validamos que lo ingresado en el input no sea un numero y que no este vacio
+  if (decipherWord.value === '' || parseInt(decipherWord.value) % 1 === 0) {
+    decipherBtn.setAttribute('disabled', true);
+  } else {
+    decipherBtn.removeAttribute('disabled');
+  }
+});
+
+// Funciones de los eventos de los botones
+function cipher() {
+  let phrase = cipherWord.value.toUpperCase();
+  let str = '';
+  for (let i = 0; i < phrase.length; i++) {
+    // nos aseguramos de que los espacios vacios no se cifren
+    if (phrase[i] === ' ') {
+      str += ' ';
+    } else {
+      // aplicamos la formula del codigo cesar y sumamos los elementos ya convertidos a las letras cifradas al string vacio y con espacios
+      str += String.fromCharCode((phrase.charCodeAt(i) - 65 + 33) % 26 + 65);
     }
   }
-  // pasamos a mayúsculas la frase que nos brindó el usuario 
-  var phrase = cipherWord.value.toUpperCase();
-  var str = '';
-
-  for (var j = 0; j < phrase.length; j++) {
-    //  hacemos una condición para los espacios entre las palabras de la frase
-    if (phrase[j] === ' ') {
-      str += ' ';
-    } else
-      // aplicamos la formula del codigo cesar y sumamos lo elementos ya convertidos a las letras cifradas al string vacio y con espacios
-      str += String.fromCharCode((phrase.charCodeAt(j) - 65 + 33) % 26 + 65);
-  }
-  //  retornamos el string que contiene las letras cifradas
-  // cipherBtn.removeAttribute(disabled);
+  // creamos una card (bootstrap 4) en el DOM para colocar la frase descifrada
   var cardCipher = document.createElement('div');
-  cardCipher.setAttribute('class', 'card');
+  cardCipher.setAttribute('class', 'card mt-2');
   var cardCipherBody = document.createElement('div');
   cardCipherBody.setAttribute('class', 'card-body');
   cardCipherBody.textContent = str;
   
   cardCipher.appendChild(cardCipherBody);
   document.getElementById('cipher-section').appendChild(cardCipher);
+
+  cipherWord.value = '';
+  cipherWord.focus();  
 }
 
-//  creamos ahora una funcion para descifrar lo ya cifrado. Vamos a enlazarlas a pedido del cliente
 function decipher() {
-  console.log('click 2');
-  console.log(decipherWord.value);
-  // declaramos un string vacio
-  var str2 = '';
-  // recorremos el string de la palabra introducida en el input del descifrado
-  for (var k = 0; k < decipherWord.value.length; k++) {
-    // si nos encontramos con un espacio en blanco en la frase
-    if (decipherWord.value[k] === ' ') {
-      // igual quedara en blanco en el string donde colocaremos el descrifrado
+  let phrase2 = decipherWord.value.toUpperCase();
+  let str2 = '';
+  for (let i = 0; i < phrase2.length; i++) {
+    // nos aseguramos de que los espacios vacios no se descifren
+    if (phrase2[i] === ' ') {
       str2 += ' ';
-    } else
-    // colocamos el descifrado del string ya cifrado de la funcion cipher. Aplicamos la formula
-      str2 += String.fromCharCode((str2.charCodeAt(k) + 65 - 33) % 26 + 65);
-  } 
-  // devolvemos el resultado del cifrado mediante un alert y el descifrado escrito en la pagina web
+    } else {
+      // aplicamos la formula
+      str2 += String.fromCharCode((phrase2.charCodeAt(i) + 65 - 33) % 26 + 65);
+    }
+  }
+  // creamos una card (bootstrap 4) en el DOM para colocar la frase descifrada
   var cardDecipher = document.createElement('div');
-  cardDecipher.setAttribute('class', 'card');
+  cardDecipher.setAttribute('class', 'card mt-2');
   var cardDecipherBody = document.createElement('div');
   cardDecipherBody.setAttribute('class', 'card-body');
   cardDecipherBody.textContent = str2;
   
   cardDecipher.appendChild(cardDecipherBody);
   document.getElementById('decipher-section').appendChild(cardDecipher);
-}
 
+  decipherWord.value = '';
+  decipherWord.focus(); 
+}
